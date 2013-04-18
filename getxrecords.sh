e8b282616242469ecddf99131cf73332871eab3b
Mace Ojala <mace@kirjakaapeli.lib.hel.fi>
2013-04-18 20:37:42 +0300

    Added a little comment what this does

--- getxrecords.sh
+++ getxrecords.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Eats the saved (from .... patroninfo/*/readinghistory), and fetches the
+# propietary XRECORD files from legacy webinterface
 
 for url in $(grep 'td.*class="patFuncTitle"' HelMet\ libraries\ _Kaikki.html |sed 's/^.*href="// ;s/?lang.*$//') ;do
 	bibid=$(echo $url |sed 's/^.*C__R//') ;
