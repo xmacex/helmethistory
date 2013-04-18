c7ae69d52a87079e272f86917b8e5eb43352be32
Mace Ojala <mace@kirjakaapeli.lib.hel.fi>
2013-04-18 20:53:13 +0300

    Take the file to parse from command line argument.

--- getxrecords.sh
+++ getxrecords.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
-# Eats the saved (from .... patroninfo/*/readinghistory), and fetches the
-# propietary XRECORD files from legacy webinterface
+#!/bin/bash
+# Eats the saved file (from .... patroninfo/*/readinghistory), and fetches the
+# propietary XRECORD files from legacy webinterface. Takes the filename as
+# argument.
 
-for url in $(grep 'td.*class="patFuncTitle"' HelMet\ libraries\ _Kaikki.html |sed 's/^.*href="// ;s/?lang.*$//') ;do
+if [ ! "$1" ] ;then
+	echo "No argument given, exiting..."
+	exit 1;
+fi
+
+htmlFile="$1"
+
+for url in $(grep 'td.*class="patFuncTitle"' "$htmlFile" |sed 's/^.*href="// ;s/?lang.*$//') ;do
 	bibid=$(echo $url |sed 's/^.*C__R//') ;
 	curl -o ${bibid}.xrecord "http://luettelo.helmet.fi/xrecord=${bibid}" ;
 done
