db29f808db07480347823613f0a9024181936e7a
Mace Ojala <mace@kirjakaapeli.lib.hel.fi>
2013-04-18 21:06:22 +0300

    Build program logic to first parse the weblist of items, them fetch the xrecord-files.

--- getxrecords.sh
+++ getxrecords.sh
@@ -9,8 +9,11 @@ if [ ! "$1" ] ;then
 fi
 
 htmlFile="$1"
+urlList=$(grep 'td.*class="patFuncTitle"' "$htmlFile" |sed 's/^.*href="// ;s/?lang.*$//')
 
-for url in $(grep 'td.*class="patFuncTitle"' "$htmlFile" |sed 's/^.*href="// ;s/?lang.*$//') ;do
-	bibid=$(echo $url |sed 's/^.*C__R//') ;
-	curl -o ${bibid}.xrecord "http://luettelo.helmet.fi/xrecord=${bibid}" ;
+for url in $urlList ;do
+	bibid=$(echo $url |sed 's/^.*C__R//')
+	bibxrecordurl="http://luettelo.helmet.fi/xrecord=${bibid}"
+	echo "Fetching record $bibid"
+	curl -o ${bibid}.xrecord $bibxrecordurl
 done
